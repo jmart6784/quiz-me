8c549c879247b9c4ff7fb1b6b7afb79bd425d39e
jmart6784 <jmartinez6784@yahoo.com>
2021-10-06 12:05:43 -0700

    Add quizes controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  root to: "homepage#index"
+  root "homepage#index"
+
+  namespace :api do
+    namespace :v1 do
+      get '/quizes/index'
+      post '/quizes/create'
+      get '/show/:id', to: 'quizes#show'
+      delete '/destroy/:id', to: 'quizes#destroy'
+      put '/update/:id', to: 'quizes#update'
+    end
+  end
 end
