a9bc9fab47a8df3b09b94894afda573309ecd25b
jmart6784 <jmartinez6784@yahoo.com>
2021-10-08 23:34:36 -0700

    Added quiz index logic to call from back end

--- app/javascript/components/quiz/QuizIndex.jsx
+++ app/javascript/components/quiz/QuizIndex.jsx
@@ -2,7 +2,40 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const QuizIndex = () => {
+  const [quizzes, setQuizzes] = useState([]);
 
+  useEffect((props) => {
+    const url = "/api/v1/quizzes/index";
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => setQuizzes(response))
+      .catch(() => props.history.push("/"));
+  }, []);
+
+  let allQuizzes = quizzes.map((quiz) => (
+    <div key={quiz.id}>
+      <p>Name: {quiz.name}</p>
+      <p>Description: {quiz.name}</p>
+    </div>
+  ));
+
+  let noQuizzes = (
+    <h1>
+      No recipes yet. Why not <Link to="/">create one</Link>
+    </h1>
+  );
+
+  return (
+    <div>
+      <h1>Quiz Index</h1>
+      {quizzes.length > 0 ? allQuizzes : noQuizzes}
+    </div>
+  );
 };
 
 export default QuizIndex;
