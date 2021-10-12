01ba86e35fb038ccd03906d3f57ced767013b248
jmart6784 <jmartinez6784@yahoo.com>
2021-10-11 19:09:46 -0700

    Added quiz show front end logic

--- app/javascript/components/quiz/QuizShow.jsx
+++ app/javascript/components/quiz/QuizShow.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const QuizShow = () => {
-  return <h1>Quiz Show</h1>
+const QuizShow = (props) => {
+  const [quiz, setQuiz] = useState("");
+
+  useEffect(() => {
+    const {
+      match: {
+        params: { id },
+      },
+    } = props;
+
+    const url = `/api/v1/quizzes/show/${id}`;
+
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => setQuiz(response))
+      .catch(() => props.history.push("/"));
+  }, []);
+
+  return (
+    <div>
+      <h1>Quiz Show</h1>
+      <h3>Name: {quiz.name}</h3>
+      <p>Description: {quiz.description}</p>
+    </div>
+  );
 };
 
 export default QuizShow;
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/quizzes/index', to: 'quizzes#index'
       post '/quizzes/create', to: 'quizzes#create'
-      get '/show/:id', to: 'quizzes#show'
+      get 'quizzes/show/:id', to: 'quizzes#show'
       delete '/destroy/:id', to: 'quizzes#destroy'
       put '/update/:id', to: 'quizzes#update'
     end
