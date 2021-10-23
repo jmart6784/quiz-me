8c30c2349b36208dcace837105d52caf244bc5c6
jmart6784 <jmartinez6784@yahoo.com>
2021-10-22 23:10:55 -0700

    added edit quiz form

--- app/javascript/components/quiz/QuizEdit.jsx
+++ app/javascript/components/quiz/QuizEdit.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 const QuizEdit = () => {
-  return <h1>Quiz Edit</h1>;
+  const [forms, setForms] = useState({
+    name: "",
+    description: "",
+  });
+
+  const onChange = (event) => {
+    const { name, value } = event.target;
+
+    setForms({ ...forms, [name]: value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <h1>Edit Quiz</h1>
+
+      <form onSubmit={onSubmit}>
+        <label htmlFor="quizName">
+          <span>Quiz Name</span>
+          <input
+            type="text"
+            name="name"
+            id="quizName"
+            required
+            onChange={onChange}
+          />
+        </label>
+
+        <label htmlFor="quizDescription">
+          <span>Description</span>
+          <input
+            type="text"
+            name="description"
+            id="quizDescription"
+            required
+            onChange={onChange}
+          />
+        </label>
+
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
 };
 
 export default QuizEdit;
