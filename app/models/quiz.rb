3ae5a22c4b6ad6a3c948b6477c3fa9f745340e45
jmart6784 <jmartinez6784@yahoo.com>
2021-11-03 20:05:13 -0700

    Added validation for active storage quiz cover file type

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,15 @@
 class Quiz < ApplicationRecord
   belongs_to :user
   has_one_attached :cover
+  validate :cover_type
+
+  private
+
+  def cover_type
+    if cover.attached?
+      if !cover.content_type.in?(%('image/jpeg image/jpg image/png'))
+        errors.add(:cover, "needs to be JPG or PNG")
+      end
+    end
+  end
 end
