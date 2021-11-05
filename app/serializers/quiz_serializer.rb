a2d5887954d618a69689fab14e56f54c85522d45
jmart6784 <jmartinez6784@yahoo.com>
2021-11-04 23:32:07 -0700

    added quiz serializer logic

--- app/serializers/quiz_serializer.rb
+++ app/serializers/quiz_serializer.rb
@@ -1,3 +1,11 @@
 class QuizSerializer < ActiveModel::Serializer
-  attributes :id
+  include Rails.application.routes.url_helpers
+  attributes :id, :name, :description, :cover
+  def cover
+    if object.cover.attached?
+      {
+        url: rails_blob_url(object.cover)
+      }
+    end
+  end
 end
