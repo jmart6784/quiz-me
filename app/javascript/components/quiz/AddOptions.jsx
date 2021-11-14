8cf9c7fd94823d4169dc19f9111206ce0de40bfd
jmart6784 <jmartinez6784@yahoo.com>
2021-11-14 14:25:06 -0800

    added div to add options title

--- app/javascript/components/quiz/AddOptions.jsx
+++ app/javascript/components/quiz/AddOptions.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const AddOptions = (props) => {
-  return <h1>Add Options</h1>;
+  return (
+    <div>
+      <h1>Add Options</h1>
+    </div>
+  );
 };
 
 export default AddOptions;
