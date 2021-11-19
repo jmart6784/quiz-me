cd735957c123ec8e1ce510ded0f56506aa65c7ed
jmart6784 <jmartinez6784@yahoo.com>
2021-11-18 22:06:58 -0800

    Removed length validation when options 3 to 10 are nil or blank

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,7 +4,9 @@ class Question < ApplicationRecord
 
   validates :question, :option_1, :option_2, :answer, :quiz_id, :user_id, presence: true
 
-  validates :question, :option_1, :option_2, :option_3, :option_4, :option_5,
+  validates :question, :option_1, :option_2, length: {minimum: 1, maximum: 300}
+
+  validates :option_3, :option_4, :option_5, 
     :option_6, :option_7, :option_8, :option_9, :option_10, 
-    length: {minimum: 1, maximum: 300}
+    length: {minimum: 1, maximum: 300}, allow_blank: true, allow_nil: true
 end
