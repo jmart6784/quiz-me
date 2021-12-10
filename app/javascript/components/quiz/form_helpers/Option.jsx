0c7de8695ecc47b7e240996cc54064ed9192cdf2
jmart6784 <jmartinez6784@yahoo.com>
2021-12-09 20:18:38 -0800

    Conditionaly rendered option required tag depending on option number

--- app/javascript/components/quiz/form_helpers/Option.jsx
+++ app/javascript/components/quiz/form_helpers/Option.jsx
@@ -4,17 +4,34 @@ const Option = (props) => {
   let ques = props.question;
   let num = props.option;
 
-  return (
-    <label htmlFor={`q${ques}_option_${num}`}>
-      <span>Option {num}</span>
-      <textarea
-        name={`q${ques}_option_${num}`}
-        rows="5"
-        required
-        onChange={props.onChange}
-      />
-    </label>
-  );
+  let optionJsx;
+
+  if (num > 2) {
+    optionJsx = (
+      <label htmlFor={`q${ques}_option_${num}`}>
+        <span>Option {num}</span>
+        <textarea
+          name={`q${ques}_option_${num}`}
+          rows="5"
+          onChange={props.onChange}
+        />
+      </label>
+    );
+  } else {
+    optionJsx = (
+      <label htmlFor={`q${ques}_option_${num}`}>
+        <span>Option {num}</span>
+        <textarea
+          name={`q${ques}_option_${num}`}
+          rows="5"
+          required
+          onChange={props.onChange}
+        />
+      </label>
+    );
+  }
+
+  return optionJsx;
 };
 
 export default Option;
