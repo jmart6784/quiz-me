cdf4c6465d89f67597c79d8dcc98efdfa5d37b3c
jmart6784 <jmartinez6784@yahoo.com>
2022-01-05 23:23:38 -0800

    Edited quiz update action to accomadate questions

--- app/controllers/api/v1/quizzes_controller.rb
+++ app/controllers/api/v1/quizzes_controller.rb
@@ -29,8 +29,15 @@ class Api::V1::QuizzesController < ApplicationController
   end
 
   def update
-    quiz&.update(quiz_params)
-    render json: {id: quiz.id, message: 'Quiz edited!'}
+    quiz.name = quiz_params[:name]
+    quiz.description = quiz_params[:description]
+
+    if quiz.valid? && update_questions(quiz.id)
+      quiz.save
+      render json: {id: quiz.id, message: 'Quiz edited!'}
+    else
+      render json: {id: quiz.id, message: 'Quiz edited!'}, status: 422
+    end
   end
 
   def destroy
@@ -98,8 +105,56 @@ class Api::V1::QuizzesController < ApplicationController
   end
 
   def update_questions(quiz_id)
+    quiz_questions = Quiz.find(quiz_id).questions
     questions = JSON.parse(quiz_params[:questions_attributes][:questions])
-    # parent_quiz = Quiz.find(quiz_id)
+
+    questions_valid = true
+
+    questions_valid = false unless questions.length > 0 && questions.length < 51
+
+    quiz_questions.each do |q_ques|
+      questions.each do |question|
+        q_ques.question_type = question["question_type"]
+        q_ques.question = question["question"]
+        q_ques.option_1 = question["option_1"]
+        q_ques.option_2 = question["option_2"]
+        q_ques.option_3 = question["option_3"]
+        q_ques.option_4 = question["option_4"]
+        q_ques.option_5 = question["option_5"]
+        q_ques.option_6 = question["option_6"]
+        q_ques.option_7 = question["option_7"]
+        q_ques.option_8 = question["option_8"]
+        q_ques.option_9 = question["option_9"]
+        q_ques.option_10 = question["option_10"]
+        q_ques.answer = JSON.generate(question["answer"])
+
+        questions_valid = false unless q_ques.valid?
+      end
+    end
+
+    if questions_valid
+      quiz_questions.each do |q_ques|
+        questions.each do |question|
+          q_ques.update(
+            question_type: question["question_type"],
+            question: question["question"],
+            option_1: question["option_1"],
+            option_2: question["option_2"],
+            option_3: question["option_3"],
+            option_4: question["option_4"],
+            option_5: question["option_5"],
+            option_6: question["option_6"],
+            option_7: question["option_7"],
+            option_8: question["option_8"],
+            option_9: question["option_9"],
+            option_10: question["option_10"],
+            answer: JSON.generate(question["answer"])
+          )
+        end
+      end
+    end
+
+    return questions_valid
   end
 
   private
