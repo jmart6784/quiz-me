e0fe0995f4d7a59562fd8c992c7017d005cc05fb
jmart6784 <jmartinez6784@yahoo.com>
2022-01-11 21:15:08 -0800

    Added update destroy and question method to question controller

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::QuestionsController < ApplicationController
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+
+  private
+
+  def question
+    @question ||= Question.find(params[:id])
+  end
 end
