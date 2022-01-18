1ea10622edb16ce809d2d561b248ea41a42221cc
jmart6784 <jmartinez6784@yahoo.com>
2022-01-17 23:09:46 -0800

    Added question edit link to quiz edit view

--- app/javascript/components/quiz/QuizEdit.jsx
+++ app/javascript/components/quiz/QuizEdit.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const QuizEdit = (props) => {
   const [forms, setForms] = useState({
@@ -112,6 +113,8 @@ const QuizEdit = (props) => {
           <p>
             {`Answer${answers.length > 1 ? "s" : ""}: ` + answers.join(", ")}
           </p>
+
+          <Link to={`/question/edit/${question.id}`}>Edit</Link>
         </div>
       );
     });
