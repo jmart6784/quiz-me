0da07d07396f179b55a099660cbc8239c1ee6411
jmart6784 <jmartinez6784@yahoo.com>
2022-01-17 23:25:14 -0800

    Requested for question data and added it to state

--- app/javascript/components/question/QuestionEdit.jsx
+++ app/javascript/components/question/QuestionEdit.jsx
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const QuestionEdit = () => {
+const QuestionEdit = (props) => {
+  const [question, setQuestion] = useState({
+    id: "",
+    question_type: "",
+    question: "",
+    option_1: "",
+    option_2: "",
+    option_3: "",
+    option_4: "",
+    option_5: "",
+    option_6: "",
+    option_7: "",
+    option_8: "",
+    option_9: "",
+    option_10: "",
+    answer: "[]",
+  });
+
+  useEffect(() => {
+    const url = `/api/v1/question/show/${props.match.params.id}`;
+
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => setQuestion(response))
+      .catch(() => props.history.push("/"));
+  }, []);
+
   return (
     <div>
       <h1>Question Edit</h1>
