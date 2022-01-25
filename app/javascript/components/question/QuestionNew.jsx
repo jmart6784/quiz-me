1069cf6a4474bd5cd63ae86c41a2e81cdfe1353f
jmart6784 <jmartinez6784@yahoo.com>
2022-01-24 22:41:08 -0800

    Added question type input field and question state object to new question view

--- app/javascript/components/question/QuestionNew.jsx
+++ app/javascript/components/question/QuestionNew.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const QuestionNew = () => {
+  const [question, setQuestion] = useState({
+    id: "",
+    question_type: "",
+    question: "",
+    option_1: "",
+    option_2: "",
+    option_3: "",
+    option_4: "",
+    option_5: "",
+    option_6: "",
+    option_7: "",
+    option_8: "",
+    option_9: "",
+    option_10: "",
+    answer: "[]",
+  });
+
   return (
     <div>
       <h1>New Question</h1>
+
+      <form>
+        <label>
+          <span>Type</span>
+          <select
+            name="question_type"
+            required
+            value={question["question_type"]}
+          >
+            <option value="one answer">Muliple choice (one answer)</option>
+            <option value="multiple answers">
+              Select all (multiple answers)
+            </option>
+          </select>
+        </label>
+      </form>
     </div>
   );
 };
