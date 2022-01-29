fcea3127d398c4fac4471d9825f7f260981273fa
jmart6784 <jmartinez6784@yahoo.com>
2022-01-29 00:10:21 -0800

    Model callback to prevent question creation for quizzes with more than 50 questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -9,4 +9,10 @@ class Question < ApplicationRecord
   validates :option_3, :option_4, :option_5, 
     :option_6, :option_7, :option_8, :option_9, :option_10, 
     length: {minimum: 1, maximum: 300}, allow_blank: true, allow_nil: true
+
+  before_create :check_question_count
+
+  def check_question_count
+    Quiz.find(self.quiz_id).questions.count < 50 ? true : false
+  end
 end
