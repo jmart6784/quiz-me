337f1e99dd26aced3587e19056108689274f64d7
jmart6784 <jmartinez6784@yahoo.com>
2022-02-03 21:59:00 -0800

    Added avatar image url to serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :first_name, :last_name, :username, :bio
+  include Rails.application.routes.url_helpers
+  attributes :id, :email, :first_name, :last_name, :username, :bio, :avatar
+
+  def avatar
+    if object.avatar.attached?
+      {
+        url: rails_blob_url(object.avatar)
+      }
+    end
+  end
 end
