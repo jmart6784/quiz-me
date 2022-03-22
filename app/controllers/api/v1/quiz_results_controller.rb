50e2085fadb86a8f75abc46f35ab3fd5f93dd26f
jmart6784 <jmartinez6784@yahoo.com>
2022-03-21 17:35:11 -0700

    Created quiz results index action

--- app/controllers/api/v1/quiz_results_controller.rb
+++ app/controllers/api/v1/quiz_results_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::QuizResultsController < ApplicationController
+  def index
+    quiz_results = QuizResult.all.order(created_at: :desc)
+    render json: quiz_results, status: 200
+  end
 end
