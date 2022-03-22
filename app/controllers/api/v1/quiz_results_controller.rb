815049e0bdd19f30b05ae807b73be46089dc5ab1
jmart6784 <jmartinez6784@yahoo.com>
2022-03-21 17:41:38 -0700

    added protected params to quiz result

--- app/controllers/api/v1/quiz_results_controller.rb
+++ app/controllers/api/v1/quiz_results_controller.rb
@@ -3,4 +3,16 @@ class Api::V1::QuizResultsController < ApplicationController
     quiz_results = QuizResult.all.order(created_at: :desc)
     render json: quiz_results, status: 200
   end
+  
+  private
+
+  def quiz_result_params
+    params.require(:quiz_result).permit(
+      :start, :end, :completed_at, :finished, :quiz_id
+    )
+  end
+
+  def quiz_result
+    @quiz_result ||= QuizResult.find(params[:id])
+  end
 end
