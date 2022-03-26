fea550f9594b6b68a2f47ba43667dd80d432d379
jmart6784 <jmartinez6784@yahoo.com>
2022-03-25 19:35:56 -0700

    loaded quiz data from back end on quiz start component

--- app/javascript/components/quiz/QuizStart.jsx
+++ app/javascript/components/quiz/QuizStart.jsx
@@ -1,6 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import secondsToTime from "./form_helpers/secondsToTime";
 
 const QuizStart = (props) => {
+  const [quiz, setQuiz] = useState({
+    id: "",
+    name: "",
+    description: "",
+    hours: "0",
+    minutes: "0",
+    seconds: "0",
+    cover: { url: "" },
+    user: {
+      id: "",
+      username: "",
+    },
+  });
+
+  useEffect(() => {
+    const url = `/api/v1/quizzes/show/${props.match.params.id}`;
+
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => {
+        let { hours, minutes, seconds } = secondsToTime(
+          parseInt(response.time)
+        );
+        setQuiz({ ...response, hours, minutes, seconds });
+      })
+      .catch(() => props.history.push("/"));
+  }, []);
+
+  useEffect(() => console.log("quizzzzz", quiz), [quiz]);
+
   return <div>Quiz Start</div>
 };
 
