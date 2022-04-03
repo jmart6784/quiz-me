7d628cdf0c2291ba7c258ab8258f558839fcf8b9
jmart6784 <jmartinez6784@yahoo.com>
2022-04-02 19:11:11 -0700

    Created validations for question result answer

--- app/models/question_result.rb
+++ app/models/question_result.rb
@@ -4,7 +4,17 @@ class QuestionResult < ApplicationRecord
   belongs_to :question
   belongs_to :quiz_result
 
-  validates :correct, :answer, :user_answer, :question_id, :quiz_id, :quiz_result_id, :user_id, presence: true
+  validates :answer, :user_answer, :question_id, :quiz_id, :quiz_result_id, :user_id, presence: true
 
   validates :correct, inclusion: [true, false]
+
+  validate :answer_type
+
+  private
+
+  def answer_type
+    answer_col = JSON.parse(answer)
+    user_answer_col = JSON.parse(user_answer)
+    throw :abort unless answer_col.class == Array && user_answer_col.class == Array
+  end
 end
