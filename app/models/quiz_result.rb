0c489e3231dc9972b254a7c711313699c463208d
jmart6784 <jmartinez6784@yahoo.com>
2022-04-02 20:20:19 -0700

    added association for question results

--- app/models/quiz_result.rb
+++ app/models/quiz_result.rb
@@ -1,6 +1,7 @@
 class QuizResult < ApplicationRecord
   belongs_to :user
   belongs_to :quiz
+  has_many :question_results, dependent: :destroy
 
   validates :start, :end, :quiz_id, :user_id, presence: true
   validates :finished, inclusion: [true, false]
