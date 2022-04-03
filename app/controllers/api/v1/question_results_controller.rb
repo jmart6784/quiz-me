e049bdb7da22fa04c733591c79903b7859596b18
jmart6784 <jmartinez6784@yahoo.com>
2022-04-02 17:54:03 -0700

    create action for question result

--- app/controllers/api/v1/question_results_controller.rb
+++ app/controllers/api/v1/question_results_controller.rb
@@ -9,7 +9,14 @@ class Api::V1::QuestionResultsController < ApplicationController
   end
 
   def create
-    
+    question_result = QuestionResult.new(question_result_params)
+    question_result.user_id = current_user.id
+
+    if question_result.save
+      render json: question_result
+    else
+      render json: question_result.errors, status: 422
+    end
   end
 
   def update
