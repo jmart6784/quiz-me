ec80541ecc0405320bb9f368c7d65ab74527cc21
jmart6784 <jmartinez6784@yahoo.com>
2022-04-02 17:47:35 -0700

    created index action for question result

--- app/controllers/api/v1/question_results_controller.rb
+++ app/controllers/api/v1/question_results_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::QuestionResultsController < ApplicationController
+  def index
+    question_results = QuestionResult.all.order(created_at: :desc)
+    render json: question_results, status: 200
+  end
+
+  def show
+    
+  end
+
+  def create
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
 end
