b6eb4956ea2d645c9e0223316779afac67b64a07
jmart6784 <jmartinez6784@yahoo.com>
2022-04-02 18:32:03 -0700

    Added basic question validations

--- app/models/question_result.rb
+++ app/models/question_result.rb
@@ -3,4 +3,8 @@ class QuestionResult < ApplicationRecord
   belongs_to :quiz
   belongs_to :question
   belongs_to :quiz_result
+
+  validates :correct, :answer, :user_answer, :question_id, :quiz_id, :quiz_result_id, :user_id, presence: true
+
+  validates :correct, inclusion: [true, false]
 end
