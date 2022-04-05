afa0d43979f87e34159d1c0215ca683499c220ab
jmart6784 <jmartinez6784@yahoo.com>
2022-04-04 21:03:45 -0700

    added has_many question_results for quiz result serializer

--- app/serializers/quiz_result_serializer.rb
+++ app/serializers/quiz_result_serializer.rb
@@ -2,4 +2,5 @@ class QuizResultSerializer < ActiveModel::Serializer
   attributes :id, :start, :end, :completed_at, :finished, :user_id, :quiz_id
   belongs_to :user
   belongs_to :quiz
+  has_many :question_results
 end
