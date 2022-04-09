433ba868769789050eb0627ce1cc7a7e754b43cd
jmart6784 <jmartinez6784@yahoo.com>
2022-04-08 20:57:36 -0700

    Created custom serializer to return questions in quiz result serializer

--- app/serializers/quiz_result_serializer.rb
+++ app/serializers/quiz_result_serializer.rb
@@ -3,4 +3,12 @@ class QuizResultSerializer < ActiveModel::Serializer
   belongs_to :user
   belongs_to :quiz
   has_many :question_results
+
+  def question_results
+    final = {question_results: object.question_results, questions: []}
+
+    final[:question_results].each { |r| final[:questions] << r.question }
+
+    final
+  end
 end
