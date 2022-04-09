cb033ff81fa2c825af2763ceadd27a1cf0fbd240
jmart6784 <jmartinez6784@yahoo.com>
2022-04-08 20:01:25 -0700

    Added question result serialzer columns and associations

--- app/serializers/question_result_serializer.rb
+++ app/serializers/question_result_serializer.rb
@@ -1,3 +1,8 @@
 class QuestionResultSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :correct, :answer, :user_answer, :question_id, :quiz_id, :quiz_result_id, :user_id
+
+  belongs_to :quiz_result
+  belongs_to :question
+  belongs_to :user
+  belongs_to :quiz
 end
