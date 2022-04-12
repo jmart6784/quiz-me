9aca7c03c0ac699bda471fcb425a89ed34002175
jmart6784 <jmartinez6784@yahoo.com>
2022-04-11 17:53:00 -0700

    requested for quiz result response in back end and impmented it into react state

--- app/javascript/components/quiz_result/QuizResultShow.jsx
+++ app/javascript/components/quiz_result/QuizResultShow.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const QuizResultShow = () => {
+const QuizResultShow = (props) => {
+  const [quizResult, setQuizResult] = useState({
+    id: "",
+    start: "",
+    end: "",
+    completed_at: "",
+    finished: false,
+    user_id: "",
+    quiz_id: "",
+    created_at: "",
+    updated_at: ""
+  });
+
+  useEffect(() => {
+    fetch(`/api/v1/quiz_results/show/${props.match.params.id}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => setQuizResult(response))
+      .catch(() => props.history.push("/"));
+  }, []);
+
+  useEffect(() => console.log(quizResult), [quizResult]);
+
   return <div><h1>QUIZ RESULT SHOW</h1></div>
 }
 
