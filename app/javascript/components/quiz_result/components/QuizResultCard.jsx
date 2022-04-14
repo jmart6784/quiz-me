9b1bbdb913cb3dcf110f14af32976c00cb790c57
jmart6784 <jmartinez6784@yahoo.com>
2022-04-13 19:46:51 -0700

    added more information to quiz result card

--- app/javascript/components/quiz_result/components/QuizResultCard.jsx
+++ app/javascript/components/quiz_result/components/QuizResultCard.jsx
@@ -2,12 +2,59 @@ import React from "react";
 import dateFormat from "../../helpers/dateFormat";
 
 const QuizResultCard = (props) => {
-  let quizResult = props.quizResult;
+  let result = props.quizResult;
+
+  let questionResults = <p>No Questions submitted</p>;
+
+  if (result.question_results.question_results.length > 0) {
+    let count = 0;
+
+    questionResults = result.question_results.question_results.map(r => {
+      count += 1;
+      let answer = JSON.parse(r.answer);
+      let userAnswer = JSON.parse(r.user_answer);
+      let question = result.question_results.questions.find(q => q.id === r.question_id);
+      let options = [];
+
+      for (let i = 1; i <= 10; i++) {
+        if (question[`option_${i}`] != "") {
+          options.push(
+            <p key={`qr-${r.id}-q-${question.id}-option-${i}`}>
+              Option {i}: {question[`option_${i}`]}
+            </p>
+          );
+        } 
+      }
+
+      return (
+        <div key={`question-result-${r.id}`}>
+          <p>Question #{count}</p>
+          <p>Question: {question.question}</p>
+          <div>{options}</div>
+          <p>{r.correct ? "Correct" : "Incorrect"}</p>
+          <p>
+            Answer: {`Answer${answer.length > 1 ? "s" : ""}: ` + answer.join(", ")}
+          </p>
+          <p>
+            Your answer: {`Answer${userAnswer.length > 1 ? "s" : ""}: ` + userAnswer.join(", ")}
+          </p>
+        </div>
+      );
+    });
+  }
 
   return (
     <div>
-      <p>Quiz name: {quizResult.quiz.name}</p>
-      <p>Completed: {dateFormat(quizResult.completed_at)[0]}</p>
+      <p>Quiz name: {result.quiz.name}</p>
+      {
+        result.finished ?
+          <p>Completed: {dateFormat(result.completed_at)[0]}</p>
+        :
+          <p>Not Complete</p>
+      }
+
+      {questionResults}
+      <br />
     </div>
   );
 };
