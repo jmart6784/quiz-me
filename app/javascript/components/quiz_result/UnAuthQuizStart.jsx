50046fd30f00abc326456799b4a87fa5e898f4e8
jmart6784 <jmartinez6784@yahoo.com>
2022-04-15 16:58:30 -0700

    Added quiz state to component

--- app/javascript/components/quiz_result/UnAuthQuizStart.jsx
+++ app/javascript/components/quiz_result/UnAuthQuizStart.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const UnAuthQuizStart = () => {
+const UnAuthQuizStart = (props) => {
+  const [quiz, setQuiz] = useState({
+    id: "",
+    name: "",
+    description: "",
+    hours: "0",
+    minutes: "0",
+    seconds: "0",
+    cover: { url: "" },
+    user: {
+      id: "",
+      username: "",
+    },
+  });
+
+  useEffect(() => console.log(quiz), [quiz]);
+
   return (
     <div>
       <h1>Unauthoized Quiz start</h1>
