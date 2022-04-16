6130eafbc930cf80970cab2fcafc1558cb0b5660
jmart6784 <jmartinez6784@yahoo.com>
2022-04-15 17:52:00 -0700

    Implemented increment and decrement question toggle

--- app/javascript/components/quiz_result/UnAuthQuizStart.jsx
+++ app/javascript/components/quiz_result/UnAuthQuizStart.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import UaOptions from "./quiz_start_helper/UaOptions";
 
 const UnAuthQuizStart = (props) => {
   const [quiz, setQuiz] = useState({
@@ -11,7 +12,26 @@ const UnAuthQuizStart = (props) => {
       id: "",
       username: "",
     },
-    questions: [],
+    questions: [
+      {
+        id: "",
+        question_type: "one answer",
+        question: "",
+        option_1: "",
+        option_2: "",
+        option_3: "",
+        option_4: "",
+        option_5: "",
+        option_6: "",
+        option_7: "",
+        option_8: "",
+        option_9: "",
+        option_10: "",
+        answer: "[]",
+        quiz_id: "",
+        user_id: ""
+      }
+    ],
   });
 
   const [page, setPage] = useState(1);
@@ -28,12 +48,35 @@ const UnAuthQuizStart = (props) => {
       .catch(() => props.history.push("/"));
   }, []);
 
+  let question = quiz["questions"][page - 1];
+
   useEffect(() => console.log(quiz), [quiz]);
 
   return (
     <div>
       <h1>Unauthorized Quiz start</h1>
-      <p>{`${page}/${quiz.questions.length}`}</p>
+      <p>Question {`${page}/${quiz.questions.length}`}</p>
+
+      <div>
+        <h3>Question #{page}</h3>
+        <p>Question: {question["question"]}</p>
+
+        <UaOptions question={question} />
+        <br />
+        <button
+          onClick={() => {
+            page > 1 ? setPage(prevState => prevState - 1) : ""
+          }}
+          disabled={page === 1}
+        >Previous</button>
+
+        <button
+          onClick={() => { 
+            quiz.questions.length >= page ? setPage(prevState => prevState + 1) : ""
+          }}
+          disabled={page === quiz.questions.length}
+        >Next</button>
+      </div>
     </div>
   );
 };
