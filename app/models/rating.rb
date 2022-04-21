a7eb0d0ff693411983e0703b99f67c78e546c34d
jmart6784 <jmartinez6784@yahoo.com>
2022-04-21 16:08:03 -0700

    Added validations and associations for rating model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,2 +1,12 @@
 class Rating < ApplicationRecord
+  belongs_to :user
+  belongs_to :quiz
+
+  validates :value, :user_id, :quiz_id, presence: true
+
+  validates :value, numericality: { 
+    only_integer: true,
+    greater_than_or_equal_to: 5,
+    less_than_or_equal_to: 1 
+  }
 end
