83a0e591bc01378deed8cfd33803050179989eae
jmart6784 <jmartinez6784@yahoo.com>
2022-04-21 15:54:43 -0700

    Added rating permitted params

--- app/controllers/api/v1/ratings_controller.rb
+++ app/controllers/api/v1/ratings_controller.rb
@@ -10,4 +10,14 @@ class Api::V1::RatingsController < ApplicationController
   def update
     
   end
+
+  private
+
+  def rating_params
+    params.require(:rating).permit(:value, :user_id, :quiz_id)
+  end
+
+  def rating
+    @rating ||= Rating.find(params[:id])
+  end
 end
