02a7360d3bd55d924e7921de29d6e0bf269eff14
jmart6784 <jmartinez6784@yahoo.com>
2022-04-29 23:25:43 -0700

    Added missing user column validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,37 @@ class User < ApplicationRecord
 
   has_one_attached :avatar, dependent: :destroy
 
+  VALID_USERNAME_REGEX = /\A[a-zA-Z0-9]+\z/
+  validates :username, uniqueness: true, presence: true, length: {minimum: 4, maximum: 16}, format: { with: VALID_USERNAME_REGEX }
+  validates :first_name, :last_name, presence: true, length: { minimum: 1, maximum: 60 }
+  validates :bio, length: { maximum: 150 }
+  validate :avatar_type
+
   after_create :set_default_avatar
 
   def set_default_avatar
     unless self.avatar.attached?
       self.avatar.attach(
+        io: File.open(
+          Rails.root.join(
+            'app', 'assets', 'images', 'default_avatar.png'
+          )
+        ), 
+        filename: 'default_avatar.png', 
+        content_type: 'image/png'
+      )
+    end
+  end
+
+  private
+
+  def avatar_type
+    if avatar.attached?
+      if !avatar.content_type.in?(%('image/jpeg image/jpg image/png'))
+        errors.add(:avatar, "needs to be JPG or PNG")
+      end
+    else
+      avatar.attach(
         io: File.open(
           Rails.root.join(
             'app', 'assets', 'images', 'default_avatar.png'
