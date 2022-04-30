ea36d8cdc91bb6b6d823e41f81fcfe49570da302
jmart6784 <jmartinez6784@yahoo.com>
2022-04-29 20:48:08 -0700

    styled pagination controls

--- app/javascript/components/layouts/Pagination.jsx
+++ app/javascript/components/layouts/Pagination.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import classnames from 'classnames';
 import { UsePagination, DOTS } from './UsePagination';
 
 const Pagination = props => {
@@ -35,46 +34,42 @@ const Pagination = props => {
   let count_id = 0;
 
    return (
-    <ul
-      className={classnames('pagination-container', { [className]: className })}
-    >
-      <button
-        className={classnames('pagination-item', {
-          disabled: currentPage === 1
+    <div className="pagination-container">
+      <div className="pagination-controls">
+         <button
+          id={currentPage === 1 ? "disabled-page-btn" : ""}
+          className="arrow-left"
+          onClick={onPrevious}
+          disabled={currentPage === 1}
+        >
+          <i className="fa-solid fa-arrow-left-long"></i>
+        </button>
+        {paginationRange.map(pageNumber => {
+          count_id += 1;
+          if (pageNumber === DOTS) {
+            return <div key={`dots-${pageNumber}-${count_id}`} className="page-dots">...</div>;
+          }
+      
+          return (
+            <button
+              key={`dots-${pageNumber}-${count_id}`}
+              className="page-number"
+              onClick={() => onPageChange(pageNumber)}
+            >
+              {pageNumber}
+            </button>
+          );
         })}
-        onClick={onPrevious}
-        disabled={currentPage === 1}
-      >
-        <div className="arrow left">ARROW LEFT</div>
-      </button>
-      {paginationRange.map(pageNumber => {
-        count_id += 1;
-        if (pageNumber === DOTS) {
-          return <li key={`dots-${pageNumber}-${count_id}`} className="pagination-item dots">...</li>;
-        }
-		
-        return (
-          <li
-            key={`dots-${pageNumber}-${count_id}`}
-            className={classnames('pagination-item', {
-              selected: pageNumber === currentPage
-            })}
-            onClick={() => onPageChange(pageNumber)}
-          >
-            {pageNumber}
-          </li>
-        );
-      })}
-      <button
-        className={classnames('pagination-item', {
-          disabled: currentPage === lastPage
-        })}
-        onClick={onNext}
-        disabled={currentPage === lastPage}
-      >
-        <div className="arrow right">ARROW RIGHT</div>
-      </button>
-    </ul>
+         <button
+          id={currentPage === lastPage ? "disabled-page-btn" : ""}
+          className="arrow-right"
+          onClick={onNext}
+          disabled={currentPage === lastPage}
+        >
+          <i class="fa-solid fa-right-long"></i>
+        </button>
+      </div>   
+    </div>
   );
 };
 
