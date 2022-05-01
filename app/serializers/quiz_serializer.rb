e668b88f389367f9a4686ad897add4b3f0fba3a7
jmart6784 <jmartinez6784@yahoo.com>
2022-05-01 10:23:37 -0700

    Added rating data to quiz serializer

--- app/serializers/quiz_serializer.rb
+++ app/serializers/quiz_serializer.rb
@@ -1,6 +1,6 @@
 class QuizSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :name, :description, :cover, :time
+  attributes :id, :name, :description, :cover, :time, :rating_data
   belongs_to :user
   has_many :questions
 
@@ -8,6 +8,31 @@ class QuizSerializer < ActiveModel::Serializer
     if object.cover.attached?
       {
         url: rails_blob_url(object.cover)
+      }
+    end
+  end
+
+  def rating_data
+    ratings = Rating.where(quiz_id: object.id)
+    h = {}
+
+    if ratings.length === 0
+      h = {
+        average: 0.0, 
+        value_1: 0, 
+        value_2: 0, 
+        value_3: 0, 
+        value_4: 0, 
+        value_5: 0
+      }
+    else
+      h = {
+        average: ratings.pluck('avg(value)').first.to_f.round(1),
+        value_1: ratings.where(value: 1).count,
+        value_2: ratings.where(value: 2).count,
+        value_3: ratings.where(value: 3).count,
+        value_4: ratings.where(value: 4).count,
+        value_5: ratings.where(value: 5).count,
       }
     end
   end
